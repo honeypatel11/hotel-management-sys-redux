5ee6987f550e073d333ccdfb1760f91d2219baea
khushbupatel05 <khushbupatel0009@gmail.com>
2025-08-05 16:15:53 +0530

    first commit home page added

---
+++ src/features/students/studentSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// const students = createSlice(()=> {
+//     name:"students",
+//     initialstate : {
+//         list:[{
+            
+//         }]
+//     },
+//     reducer:{
+
+//     }
+// })
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <section className="bg-img h-screen flex items-center ">
+      <div className="container mx-auto">
+        <h1 className="text-8xl fw-bold text-white mb-6">Explore the <br /> World Now!</h1>
+        <p className="text-xl text-white max-w-xl ">
+          Looking for your dream vacation destination but don't know where to start?
+          With the help of our hotel agents, you can plan the trip of a lifetime with ease.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
